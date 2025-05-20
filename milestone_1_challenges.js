7c1f904e560fc2887b70959c55362dbe9e421a33
briannjoroge <briannjoroge007@gmail.com>
2025-05-20 23:05:56 +0300

    completed 1. Return the Sum of Two Numbers

---
+++ milestone_1_challenges.js
@@ -0,0 +1,9 @@
+//1. Return the Sum of Two Numbers
+let num1, num2;
+function addition(num1, num2) {
+    let sum = num1 + num2;
+    console.log(`${num1} + ${num2} = ${sum}`)
+}
+
+addition(3,5)
+addition(-6,9)
