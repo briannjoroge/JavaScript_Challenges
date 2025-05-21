3308fc8349e6cc6aa999100d1d4e7411454bcdd1
briannjoroge <briannjoroge007@gmail.com>
2025-05-22 01:11:48 +0300

    completed 3. Election winner

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -13,14 +13,33 @@ sumOfPositives([1, -3, 5, -2, 9, -8]);
 
 //2. Find Maximum Value
 function findMax(bigger) {
-    let big = bigger[0];
+  let big = bigger[0];
   for (let i = 0; i < bigger.length; i++) {
     if (bigger[i] >= big) {
-        big = bigger[i];
-    } 
+      big = bigger[i];
+    }
   }
-  console.log(`The maximum value is ${big}`)
+  console.log(`The maximum value is ${big}`);
 }
 
-findMax([2,5,8,7,6,10])
-findMax([-3.-7,0,9,4,9,2])
+findMax([2, 5, 8, 7, 6, 10]);
+findMax([-3 - 7, 0, 9, 4, 9, 2]);
+
+//3. Election winner
+const candidates = [
+  { name: "Alice", votes: 50 },
+  { name: "Bob", votes: 75 },
+  { name: "Charlie", votes: 65 },
+];
+
+function findWinner(candidates) {
+  let winner = candidates[0];
+  for (let i = 0; i < candidates.length; i++) {
+    if (candidates[i].votes > winner.votes) {
+      winner = candidates[i];
+    }
+  }
+  return winner;
+}
+
+console.log(findWinner(candidates));
