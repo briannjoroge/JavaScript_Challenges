2592d1bb4f1a3c198b2e4b79cf1ada5c6f962fc2
briannjoroge <briannjoroge007@gmail.com>
2025-06-02 08:58:05 +0300

    completed 1. Return the Sum of Two Numbers

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function addition(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+console.log(addition(3, 5));
+console.log(addition(-6, 9));
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+//1. Return the Sum of Two Numbers
+function addition(num1: number, num2: number): number {
+  let sum = num1 + num2;
+  return sum;
+}
+
+console.log(addition(3, 5));
+console.log(addition(-6, 9));
