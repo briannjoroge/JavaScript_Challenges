a7350d40dbbb9744d5505698f758e228aa273daf
briannjoroge <briannjoroge007@gmail.com>
2025-06-02 10:50:38 +0300

    completed 2. Convert Minutes into Seconds

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
 function addition(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
+  let sum = num1 + num2;
+  return sum;
 }
 console.log(`3 + 5 = ${addition(3, 5)}`);
 console.log(`-6 + 9 = ${addition(-6, 9)}`);
+function convert(min) {
+  let secondd = min * 60;
+  return secondd;
+}
+console.log(` 5 min = ${convert(5)}s`);
+console.log(` 1 min = ${convert(1)}s`);
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,12 @@ function addition(num1: number, num2: number): number {
 
 console.log(`3 + 5 = ${addition(3, 5)}`);
 console.log(`-6 + 9 = ${addition(-6, 9)}`);
+
+//2. Convert Minutes into Seconds
+function convert(min: number): number {
+  let secondd = min * 60;
+  return secondd;
+}
+
+console.log(` 5 min = ${convert(5)}s`);
+console.log(` 1 min = ${convert(1)}s`);
